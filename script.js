ff9ea7ccc97357a882a62db108bd2045f4934d38
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-04-23 13:03:17 +0200

    Rettet design så det er tilgiveligt, rettet footer, kommenteret CSS

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ document.addEventListener("DOMContentLoaded", start);
 let restapi = [];
 let page = "";
 let primaryTopicName = "";
+let secondaryTopicName = "";
+let subNavButton = document.querySelectorAll(".sub_nav_item");
+
 let urlParams = new URLSearchParams(window.location.search);
 let primaryID = urlParams.get("primaryID");
+let secondaryID = urlParams.get("secondaryID");
 
 
 function start() {
+    fixTitles();
+    function fixTitles() {
+    //MAIN PAGES
     if (primaryID === "stevnsfort") {
         primaryTopicName = "Koldkrigsmuseum Stevnsfort";
     }
@@ -20,19 +27,44 @@ function start() {
         primaryTopicName = "Geomuseum Faxe";
     }
 
-    console.log("I'm starting");
-    console.log("primaryID = ", primaryID);
+    //SUB PAGES
+    if (secondaryID === "about") {
+        secondaryTopicName = "Om";
+    }
+
+    if (secondaryID === "visit") {
+        secondaryTopicName = "Besøg os";
+    }
+
+    if (secondaryID === "teaching") {
+        secondaryTopicName = "Undervisning";
+    }
+
+    if (secondaryID === "handicap") {
+        secondaryTopicName = "Info for handicappede";
+    }
+
+    if (secondaryID === "groups") {
+        secondaryTopicName = "For grupper";
+    }
+
+    if (secondaryID === "find") {
+        secondaryTopicName = "Find vej";
+    }
+    }
+
+    console.log("start()");
     async function getJson() {
-        let url = "https://camillagejl.com/kea/2-semester/tema7/kalklandet/wordpress/wp-json/wp/v2/pages/";
-        let jsonData = await fetch(url);
+        let pagesUrl = "https://camillagejl.com/kea/2-semester/tema7/kalklandet/wordpress/wp-json/wp/v2/pages/";
+        let jsonData = await fetch(pagesUrl);
         restapi = await jsonData.json();
         restapi.forEach(obj => {
 
-            console.log("I'm here", obj);
             console.log("primaryID = ", primaryID);
+            console.log("secondaryID = ", secondaryID);
             console.log("Primary topic = ", obj.primary_topic[0]);
-            if (primaryTopicName === obj.primary_topic[0]) {
-                console.log("cool bro");
+            if (primaryTopicName === obj.primary_topic[0] && secondaryTopicName === obj.secondary_topic[0]) {
+                console.log("JEG MATCHER!");
                 page = obj;
             }
         });
@@ -40,6 +72,10 @@ function start() {
     }
 
     function designPage() {
+
+        subNavButton.forEach(button => {
+        button.classList.remove("hidden");
+        });
 
         let mainDesign = document.querySelectorAll(".main_design");
         console.log(page);
@@ -57,8 +93,16 @@ function start() {
     function showContent() {
         let dest = document.querySelector("main");
         document.querySelector(".page_title").textContent = primaryTopicName;
-        dest.querySelector("h1").textContent = page.title.rendered;
-        dest.querySelector("#text_content").innerHTML = page.content.rendered;
+
+        if (secondaryTopicName === "Om") {
+            dest.querySelector("h1").textContent = "Velkommen til " + primaryTopicName;
+        }
+
+        else {
+            dest.querySelector("h1").textContent = secondaryTopicName;
+        }
+
+        dest.querySelector("#text_content").innerHTML += page.content.rendered;
         dest.querySelector("img").src = page.image[0].guid;
 
     }
@@ -67,10 +111,21 @@ function start() {
     mainNavButton.forEach(button => {
         button.addEventListener("click", function () {
             let primaryAttribute = this.getAttribute("data-primary_attribute");
-            location.href = "om.html?primaryID=" + primaryAttribute;
+            let secondaryAttribute = this.getAttribute("data-secondary_attribute");
+            location.href = "page.html?primaryID=" + primaryAttribute + "&secondaryID=" + secondaryAttribute;
             console.log(primaryAttribute);
         });
     });
+
+    subNavButton.forEach(button => {
+        button.addEventListener("click", function () {
+            let secondaryAttribute = this.getAttribute("data-secondary_attribute");
+            location.href = "page.html?primaryID=" + primaryID + "&secondaryID=" + secondaryAttribute;
+            console.log(primaryAttribute);
+        });
+    });
+
+
 
     getJson();
 }
